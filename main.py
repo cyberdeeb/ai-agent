58b070462985683b761aa4323677e32cc52d7c64
cyberdeeb <abrahamdeeb@gmail.com>
2025-06-30 11:18:09 -0700

    Add user input

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
 
-prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-content = client.models.generate_content('gemini-2.0-flash-001', prompt)
+def main():
+    load_dotenv()
 
-print(content.text, content.usage_metadata)
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    content = sys.argv[1]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=content,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
